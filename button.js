cf38524329b48c901feff447f2a70d9d35891d5f
Gusstavofernandes <gustavofoliveira153@gmail.com>
2025-11-04 14:26:50 -0300

    att project

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
 const prevButton = document.getElementById('prev')
 const nextButton = document.getElementById('next')
-const items = document.querySelectorAll('.item')
+const items = document.querySelectorAll('.text')
+const list = document.querySelector('.list')
 
 let active = 0;
 const total = items.length
@@ -8,11 +9,31 @@ let timer;
 
 function update(direction){
     document.querySelector('.text.active').classList.remove('active')
-}
 
-prevButton.addEventListener('click', function(){
+    if(direction > 0){
+        active = active + 1
+        if(active === total){
+            active = 0
+        }
+    }
+
+    else if(direction < 0){
+        active = active - 1
+        if(active < 0){
+            active = total-1
+        }
+    }
+    items[active].classList.add('active')
+
+}
+clearInterval(timer)
+timer = setInterval(() => {
+        update(1)
+    }, 10000);
+
+prevButton.addEventListener('click', () =>{
     update(-1)
 })
-nextButton.addEventListener('click', function(){
+nextButton.addEventListener('click', () =>{
     update(1)
 })
